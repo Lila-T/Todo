2cdb69198912439f1b206b1e81779b7115ce8559
Lila-T <42620211+Lila-T@users.noreply.github.com>
2018-08-29 23:37:33 +0430

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Todo
+# Todo List 
+Todo List is list of all the tasks that you need to carry out. By keeping such a list, you make sure that your tasks are written down all
+in one place so you don’t forget anything important.
+
+## Getting started
+The project setup is really simple. Because we only used pure JavaScript, So you do not need to install frameworks or dependencies. The  “todo.html” file is the entry point and contains the HTML code. So you Just need to open it.
+
---
+++ todo.js
@@ -0,0 +1,94 @@
+var todoListContainer = document.getElementById('todoListContainer'),
+    newTodo = document.getElementById('newTodo'),
+    errorMsg = document.getElementById('errorMsg'),
+    todoList=[];
+
+init();
+
+function init() {
+	newTodo.focus();
+}
+
+function createTodoElement(todo, index){
+	var el = document.createElement('div'),
+	    todoEl = document.createElement('span'),
+	    deleteBtn = document.createElement('button'),
+	    doneBtn = document.createElement('button');
+
+	todoEl.innerHTML = todo + '&nbsp;'
+	todoEl.className='todoEl';
+	
+	deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
+	deleteBtn.title='Delete';
+	deleteBtn.className='btn btn-delete';
+	deleteBtn.onclick = function(){
+		deleteTodo(todo, el);
+	};
+	
+	doneBtn.title = 'Done';
+	doneBtn.className = 'btn btn-done';
+	doneBtn.onclick = function(){
+		doneTodo(todoEl, doneBtn);
+	};
+	
+	el.appendChild(todoEl);
+	el.appendChild(deleteBtn);
+	el.appendChild(doneBtn);
+	todoListContainer.appendChild(el);
+}
+	
+
+function addNewTodo(){
+	clearErrorMsg();
+	var result = checkDuplicate(newTodo.value);
+	if(result !== undefined ){
+		errorMsg.innerHTML = 'Duplicated Item';
+		return;
+	}
+	if(newTodo.value == ''){
+	errorMsg.innerHTML = 'Please enter valid item';
+		return;
+	}
+	var warningMsgEl = document.getElementById('warningMsg');
+	if(document.getElementById('warningMsg')){
+		todoListContainer.removeChild(warningMsgEl);
+	}
+	todoList.push(newTodo.value);
+
+	createTodoElement(newTodo.value);
+	newTodo.value = '';
+	newTodo.focus();
+}
+
+function deleteTodo(todo, el){
+	var result = confirm("Do you Want to delete '"+ todo+ "' ?");
+	if (result) {
+		todoList.splice(todoList.indexOf(todo), 1);
+		todoListContainer.removeChild(el);
+		
+		if(todoList.length == 0 ){
+			var msg= document.createElement('div');
+			msg.id='warningMsg';
+			msg.className= 'warning-msg';
+			msg.innerHTML= 'No item is availeble!';
+			todoListContainer.appendChild(msg);
+		}
+	}
+	else return ;
+}
+
+function doneTodo(todoEl,doneBtn ){
+	var result = confirm("Do you Want to done '"+ todoEl.innerText+ "' ?");
+	if (result) {
+		todoEl.style.color = 'green';
+		doneBtn.className= 'btn disable-done-btn';
+	}
+}
+function clearErrorMsg(){
+	errorMsg.innerHTML='';
+}
+function checkDuplicate(todo){
+	return todoList.find(function (item){
+		return item == todo;
+	});
+}
